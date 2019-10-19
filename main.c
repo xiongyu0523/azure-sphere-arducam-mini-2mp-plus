ddcd2758adab1e9a36ce9bc8a2205b89fe228694
neo <xiongyu0523@gmail.com>
2019-10-19 14:48:11 +0800

    empty project template

---
+++ main.c
@@ -0,0 +1,43 @@
+﻿#include <stdbool.h>
+#include <errno.h>
+#include <string.h>
+#include <time.h>
+
+#include <applibs/log.h>
+#include <applibs/gpio.h>
+
+int main(void)
+{
+    // This minimal Azure Sphere app repeatedly toggles GPIO 9, which is the green channel of RGB
+    // LED 1 on the MT3620 RDB.
+    // If your device exposes different GPIOs, you might need to change this value. For example,
+    // to run the app on a Seeed mini-dev kit, change the GPIO from 9 to 7 in the call to
+    // GPIO_OpenAsOutput and in the app_manifest.json to blink its LED. Check with your hardware
+    // manufacturer to determine which GPIOs are available.
+    // Use this app to test that device and SDK installation succeeded that you can build,
+    // deploy, and debug an app with Visual Studio, and that you can deploy an app over the air,
+    // per the instructions here: https://docs.microsoft.com/azure-sphere/quickstarts/qs-overview
+    //
+    // It is NOT recommended to use this as a starting point for developing apps; instead use
+    // the extensible samples here: https://github.com/Azure/azure-sphere-samples
+    Log_Debug(
+        "\nVisit https://github.com/Azure/azure-sphere-samples for extensible samples to use as a "
+        "starting point for full applications.\n");
+
+    // Change this GPIO number and the number in app_manifest.json if required by your hardware.
+    int fd = GPIO_OpenAsOutput(9, GPIO_OutputMode_PushPull, GPIO_Value_High);
+    if (fd < 0) {
+        Log_Debug(
+            "Error opening GPIO: %s (%d). Check that app_manifest.json includes the GPIO used.\n",
+            strerror(errno), errno);
+        return -1;
+    }
+
+    const struct timespec sleepTime = {1, 0};
+    while (true) {
+        GPIO_SetValue(fd, GPIO_Value_Low);
+        nanosleep(&sleepTime, NULL);
+        GPIO_SetValue(fd, GPIO_Value_High);
+        nanosleep(&sleepTime, NULL);
+    }
+}
